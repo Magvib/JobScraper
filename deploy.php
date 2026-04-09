073686ce75389a3108fba4604c70a1014aad35ff
Magnus Nielsen <Magnus@p13.dk>
2026-04-09 20:51:28 +0200

    feat: Add deployment configuration for JobScraper using Deployer

---
+++ deploy.php
@@ -0,0 +1,32 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/Magvib/JobScraper.git');
+set('node_version', '23');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('jobscraper.work')
+    ->set('remote_user', 'root')
+    ->set('hostname', '46.225.146.106')
+    ->set('deploy_path', '~/JobScraper');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
+
+after('deploy:update_code', function () {
+    run('cd {{release_path}} && npm install && npm run build');
+});
+
+before('provision:update', function () {
+    run('apt install -y gh npm');
+});
