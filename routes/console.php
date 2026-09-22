a5d29327d410a2af1360be3a2dd35e56f91bd468
Magnus <mbn@ditur.dk>
2026-09-22 10:26:34 +0200

    Update queue worker schedule: Change frequency from every thirty seconds to every minute for improved efficiency.

--- routes/console.php
+++ routes/console.php
@@ -13,5 +13,5 @@ Schedule::command('jobs:auto-match')
     ->withoutOverlapping();
 
 Schedule::command('queue:work --stop-when-empty')
-    ->everyThirtySeconds()
+    ->everyMinute()
     ->withoutOverlapping();
